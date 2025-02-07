fdac410efcd064c6114d6c7f33fb17e048e8a39f
Thiago Peixoto dos Santos <thiago.peixots@gmail.com>
2025-02-07 08:20:52 -0300

    Fix method

--- src/main/java/org/example/introduce/BinarySearch.java
+++ src/main/java/org/example/introduce/BinarySearch.java
@@ -1,28 +1,29 @@
 package org.example.introduce;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BinarySearch {
     List<Integer> numbersOrd;
 
-    public BinarySearch(List<Integer> numbersOrd){
+    public BinarySearch(List<Integer> numbersOrd) {
         this.numbersOrd = numbersOrd;
     }
 
-    public int binaryS (List<Integer> numbersOrd, int target){
+    public int binaryS (int target){
         int low = 0;
         int high = numbersOrd.size() - 1;
-        int mid = low + high / 2;
-        while (low >= high){
 
+
+        while (low <= high)  {
+            int mid = (low + high) / 2;
             if (target == numbersOrd.get(mid)){
                 return mid;
-            } else if (target < numbersOrd.get(mid)){
+            } else if (target < numbersOrd.get(mid)) {
                 high = mid - 1;
-            } else{
+            } else {
                 low = mid + 1;
             }
-
         }
         return -1;
     }
