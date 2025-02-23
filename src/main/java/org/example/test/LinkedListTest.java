2020218977392b48dd8ea0a3f570d6416105de9d
Thiago Peixoto <thiago.peixots@gmail.com>
2025-02-23 12:05:56 -0300

    create test for LinkedList

---
+++ src/main/java/org/example/test/LinkedListTest.java
@@ -0,0 +1,13 @@
+package org.example.test;
+
+import org.example.introduce.LinkedList;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addElement(1);
+
+        System.out.println(list);
+
+    }
+}
