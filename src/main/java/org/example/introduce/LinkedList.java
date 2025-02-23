8603bc8d1e94f3a00f80e983349faa8ba59ca000
Thiago Peixoto <thiago.peixots@gmail.com>
2025-02-23 16:16:20 -0300

    create a suplement to LinkedList

--- src/main/java/org/example/introduce/LinkedList.java
+++ src/main/java/org/example/introduce/LinkedList.java
@@ -4,12 +4,20 @@ import org.example.modules.Node;
 
 public class LinkedList<T> {
     private Node<T> init;
+    private Node<T> last;
+
     private int size;
 
     public void addElement (T element){
         Node<T> celula = new Node<T>(element);
-        this.init = celula;
-        this.size++;
+        size++;
+
+        if (size >= 1){
+            this.last.setNext(celula);
+        } else {
+            this.init = celula;
+        }
+        this.last=celula;
     }
 
     public int getSize() {
