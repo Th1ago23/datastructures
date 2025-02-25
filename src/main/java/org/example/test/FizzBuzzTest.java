da0c52bfd7cb359869ed206629e4931a7995bb00
Thiago Peixoto dos Santos <thiago.peixots@gmail.com>
2025-02-25 08:42:32 -0300

    Implemente fizzbuzz test ✓

---
+++ src/main/java/org/example/test/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+package org.example.test;
+
+import org.example.exercises.FizzBuzz;
+
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz fizz = new FizzBuzz(100);
+        fizz.count();
+    }
+}
