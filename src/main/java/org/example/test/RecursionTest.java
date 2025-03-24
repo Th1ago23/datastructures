3b22303ce9d1c5ea7ec408614f2bb7f7d4868c16
Thiago Peixoto dos Santos <thiago.peixots@gmail.com>
2025-03-24 11:30:05 -0300

    Implement Recursion Test

---
+++ src/main/java/org/example/test/RecursionTest.java
@@ -0,0 +1,10 @@
+package org.example.test;
+
+import org.example.exercises.Recursion;
+
+public class RecursionTest {
+    public static void main(String[] args) {
+        Recursion rec = new Recursion(20);
+        rec.returnUntilN(20, 1);
+    }
+}
