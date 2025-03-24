91aec1e4c47c217e2983d18a546d66d123ccb1d9
Thiago Peixoto dos Santos <thiago.peixots@gmail.com>
2025-03-24 11:23:10 -0300

    Implement Recursion

---
+++ src/main/java/org/example/exercises/Recursao.java
@@ -0,0 +1,38 @@
+package org.example.exercises;
+
+public class Recursao {
+    private int number;
+
+
+
+    public Recursao(int number){
+        this.number = number;
+    }
+
+    public void returnUntilOne (int number){
+        if (number <= 1){
+            System.out.println("1");
+        }
+            System.out.println(number);
+            returnUntilOne(number / 2);
+        }
+
+    public void returnUntilN (int number, int target){
+        if (target == number){
+            System.out.println(1);
+        }
+        System.out.println("O target é: "+target + " Contagem atual: " + number);
+        returnUntilN(number, target + 1);
+    }
+
+    public int fatorialN(int number){
+        if (number == 1 ){
+            return 1;
+        }
+
+        return number + fatorialN(number*(number - 1));
+        }
+    }
+
+
+
