ab3914b9bece95b815622230c3f58d204c00f78e
Thiago Peixoto dos Santos <thiago.peixots@gmail.com>
2025-03-24 10:11:28 -0300

    Implement Big O knowledge

---
+++ src/main/java/org/example/exercises/BigO.java
@@ -0,0 +1,17 @@
+package org.example.exercises;
+
+public class BigO {
+    private int target;
+
+    public BigO ( int target){
+        this.target = target;
+    }
+
+    public int getTarget(){
+        return target;
+    }
+
+    public void setTarget(int target){
+        this.target = target;
+    }
+}
